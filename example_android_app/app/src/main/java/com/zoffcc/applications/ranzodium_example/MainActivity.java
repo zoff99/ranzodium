10fb3a77f8ea30152544bf3c24444ca2f0bc695c
zoff99 <zoff99@users.noreply.github.com>
2025-08-15 19:55:42 +0200

    simple screen with text

--- example_android_app/app/src/main/java/com/zoffcc/applications/ranzodium_example/MainActivity.java
+++ example_android_app/app/src/main/java/com/zoffcc/applications/ranzodium_example/MainActivity.java
@@ -1,7 +1,22 @@
 package com.zoffcc.applications.ranzodium_example;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity
 {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView t = this.findViewById(R.id.text_window);
+        t.setText("...");
+        t.setVisibility(View.VISIBLE);
+    }
 }
