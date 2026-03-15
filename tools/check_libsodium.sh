58874a5333ed010dfbf15c53e93491021a74d5c6
zoff99 <zoff99@users.noreply.github.com>
2026-03-15 13:29:32 +0100

    add update checker for libsodium

---
+++ tools/check_libsodium.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+r1='https://github.com/jedisct1/libsodium'
+ver=$(git ls-remote --refs --sort='v:refname' --tags "$r1" \
+    | cut --delimiter='/' --fields=3 | grep '\-RELEASE' | tail --lines=1)
+
+echo "__VERSIONUPDATE__:""$ver"
+
+d='./build_scripts/'
+f='deps.sh'
+
+cd "$d"
+grep -ril '_LIBSODIUM_VERSION_='|xargs -L1 sed -i -e 's#_LIBSODIUM_VERSION_=".*"#_LIBSODIUM_VERSION_="'"$ver"'"#'
